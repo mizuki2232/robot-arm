7c3e49d5271982fd0ad4c142da3026c8bfddd72d
mizuki2232 <mizuki.kamata@serverworks.co.jp>
2017-08-06 20:10:48 +0900

    add opencv script

--- aws-lambda-python-opencv-master/lambda-package/lambda_function.py
+++ aws-lambda-python-opencv-master/lambda-package/lambda_function.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import boto3
 import cv2
 
@@ -7,7 +9,16 @@ s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     s3.Bucket('bento-robot').download_file('girl_laugh_face.jpg', '/tmp/test.jpg')
-    return "It works!"
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# if __name__ == "__main__":
-#     lambda_handler(42, 42)
+    img = cv2.imread('/tmp/test.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv2.imwrite('/tmp/modified.jpg', img)
+    data = open('/tmp/modified.jpg', 'rb')
+    s3.Bucket('bento-robot').put_object('girl_laugh_face_modifie.jpg', data)
+
+    return "It works!"
