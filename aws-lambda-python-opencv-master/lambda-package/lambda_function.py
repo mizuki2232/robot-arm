49d4f86c73124380ba52cbfc6ae5d065404a88b7
mizuki2232 <mizuki.kamata@serverworks.co.jp>
2017-08-10 07:21:42 +0900

    add face_tracking.py

--- aws-lambda-python-opencv-master/lambda-package/lambda_function.py
+++ aws-lambda-python-opencv-master/lambda-package/lambda_function.py
@@ -3,20 +3,43 @@ import cv2
 
 
 s3 = boto3.resource('s3')
+sqs = boto3.resource('sqs')
+queue_name = 'robot_arm'
+
+try:
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+except:
+    queue = sqs.create_queue(QueueName=queue_name)
 
 
 def lambda_handler(event, context):
-    s3.Bucket('bucket_name).download_file('girl.jpg', '/tmp/test.jpg')
+    s3.Bucket('bento-robot').download_file('capture.jpg', '/tmp/capture.jpg')
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-    img = cv2.imread('/tmp/test.jpg')
+    img = cv2.imread('/tmp/capture.jpg')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
-    cv2.imwrite('/tmp/modified.jpg', img)
-    data = open('/tmp/modified.jpg', 'rb')
-    s3.Bucket('bucket_name').put_object(Key ='girl_modified.jpg', Body = data)
+    height = img.shape[0]
+    width = img.shape[1]
+    frame_top = height/4
+    frame_left = width/4
+    frame_right = width - width/4
+    frame_bottom = height - height/4
+    order = "order="
 
-    return "It works!"
+    for (x, y, w, h) in faces:
+        # cv2.rectangle(image,(top-left point),(bottom-right point),(color),bold line)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    if x < frame_left:
+        order += "turn right!, "
+    if x + w > frame_right:
+        order += "turn left!, "
+    if y < frame_top:
+        order += "turn bottom!, "
+    if y + h > frame_bottom:
+        order += "turn top!, "
+
+    response = queue.send_message(MessageBody=order)
+
+    return response
--- face_tracking.py
+++ face_tracking.py
@@ -29,9 +29,15 @@ servo4 = GPIO.PWM(PIN, 50)
 val = [2.5,3.6875,4.875,6.0625,7.25,8.4375,9.625,10.8125,12]
 
 
-capture_image = "test.png"
+capture_image = "capture.jpg"
 bucket_name = "bento_robot"
 s3 = boto3.resource('s3')
+sqs = boto3.resource('sqs')
+queue_name = 'robot_arm'
+try:
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+except:
+    queue = sqs.create_queue(QueueName=queue_name)
 
 
 class worker():
@@ -45,40 +51,33 @@ class worker():
         print "uploading to S3..."
         s3.Bucket(bucket_name).upload_file('./' + capture_image, capture_image)
 
-        time.sleep(10)
-
-    def check_order():
-        """check Amazon sqs queue"""
-        # check sqs job
-        #if job in queue
-        if 1 """sqs has order""" == True:
-            order1 = 1
-            order2 = 1
-            order3 = 1
-            order4 = 1
-            time.sleep(3)
+    def get_order():
+        """Get Amazon SQS Message"""
+        if order = queue.receive_message() == True:
             return True
+        else:
+            return False
 
     def control_servo():
-        """Control Servo it subject to sqs orders"""
+        """Control Servo it subject to Amazon SQS orders"""
         servo1.start(0.0)
         servo2.start(0.0)
         servo3.start(0.0)
         servo4.start(0.0)
 
-        servo1.ChangeDutyCycle(val[check_order.order1])
-        time.sleep(0.3)
-        servo2.ChangeDutyCycle(val[check_order.order2])
-        time.sleep(0.3)
-        servo3.ChangeDutyCycle(val[check_order.order3])
-        time.sleep(0.3)
-        servo4.ChangeDutyCycle(val[check_order.order4])
-        time.sleep(0.3)
+        if get_order.order in "turn right":
+            servo1.ChangeDutyCycle(val[0])
+        if get_order.order in "turn left":
+            servo1.ChangeDutyCycle(val[8])
+        if get_order.order in "turn bottom":
+            servo4.ChangeDutyCycle(val[0])
+        if get_order.order in "turn top":
+            servo4.ChangeDutyCycle(val[8])
 
 
 while True:
     worker.upload_image()
     while True:
-        if worker.check_order() == True:
+        if worker.get_order() == True:
             worker.control_servo()
             break;
