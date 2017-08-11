585ada52468ccd2d5b83bd5cfa076ba622bfeb21
Mizuki Kamata <mizuki255@gmail.com>
2017-08-11 05:34:00 +0000

    modify bucket name

--- face_tracking.py
+++ face_tracking.py
@@ -29,7 +29,7 @@ val = [2.5, 3.6875, 4.875, 6.0625, 7.25, 8.4375, 9.625, 10.8125, 12]
 
 
 capture_image = "capture.jpg"
-bucket_name = "bento_robot"
+bucket_name = "bento-robot"
 s3 = boto3.resource('s3')
 sqs = boto3.resource('sqs')
 queue_name = 'robot_arm'
