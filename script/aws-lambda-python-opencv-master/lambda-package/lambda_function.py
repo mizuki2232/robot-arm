9daad7c44df039b657985a85644920256649b3aa
mizuki2232 <mizuki.kamata@serverworks.co.jp>
2017-08-19 20:37:38 +0900

    change directory and worker can publish image binary to sqs

---
+++ script/aws-lambda-python-opencv-master/lambda-package/lambda_function.py
@@ -0,0 +1,55 @@
+import boto3
+import cv2
+import json
+
+
+s3 = boto3.resource('s3')
+sqs = boto3.resource('sqs')
+queue_name = 'robot_arm'
+image_file = 'capture.jpg'
+order = {}
+
+try:
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+except:
+    queue = sqs.create_queue(QueueName=queue_name)
+
+
+def lambda_handler(event, context):
+    s3.Bucket('bento-robot').download_file(image_file, '/tmp/' + image_file)
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    img = cv2.imread('/tmp/' + image_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 3)
+
+    height = img.shape[0]
+    width = img.shape[1]
+    img_center_x = width / 2
+    img_center_y = height / 2
+
+    for (x, y, w, h) in faces:
+        # cv2.rectangle(image,(top-left point),(bottom-right point),(color),bold line)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        obj_center_x = x + w / 2
+        obj_center_y = y + h / 2
+
+
+    x_distance = img_center_x - obj_center_x
+    y_distance = img_center_y - obj_center_y
+
+    if x_distance > 0:
+        order = {"turn_left" : x_distance}
+    if x_distance < 0:
+        order = {"turn_right" : abs(x_distance)}
+    if y_distance > 0:
+        order = {"turn_top" : y_distance}
+    if y_distance > 0:
+        order = {"turn_bottom" : abs(y_distance)}
+
+    if obj_center_x:
+        body = json.dumps(order)
+        response = queue.send_message(MessageBody=body)
+        return response
+    else:
+        return "Nothing to do."
---
+++ script/dlib/worker.py
@@ -0,0 +1,158 @@
+import json
+import time
+import io
+
+import boto3
+import cv2
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+
+PIN = 23
+GPIO.setup(PIN, GPIO.OUT)
+servo1 = GPIO.PWM(PIN, 50)
+
+PIN = 24
+GPIO.setup(PIN, GPIO.OUT)
+servo2 = GPIO.PWM(PIN, 50)
+
+PIN = 25
+GPIO.setup(PIN, GPIO.OUT)
+servo3 = GPIO.PWM(PIN, 50)
+
+PIN = 8
+GPIO.setup(PIN, GPIO.OUT)
+servo4 = GPIO.PWM(PIN, 50)
+
+val = [2.5, 3.6875, 4.875, 6.0625, 7.25, 8.4375, 9.625, 10.8125, 12]
+
+capture_image = "capture.jpg"
+sqs = boto3.resource('sqs')
+
+try:
+    order_queue = sqs.get_queue_by_name(QueueName='robot_arm_order')
+except:
+    order_queue = sqs.create_queue(QueueName='robot_arm_order')
+
+try:
+    image_queue = sqs.get_queue_by_name(QueueName='robot_arm_image')
+except:
+    image_queue = sqs.create_queue(QueueName='robot_arm_image')
+
+
+class Worker:
+    order = ''
+    current_point = [6, 2]
+
+    def publish_queue(self):
+        """Publish Queue"""
+        print "Take Picture..."
+        c = cv2.VideoCapture(0)
+        r, img = c.read()
+        img_bin = io.BytesIO(img)
+        c.release()
+        print ""
+        print "Publish Queue"
+        response = image_queue.send_message(MessageBody=img_bin)
+        print response
+
+    def get_order(self):
+        """Get Amazon SQS Message"""
+        print ""
+        try:
+            message = queue.receive_messages(
+                AttributeNames=[
+                    'All'
+                ],
+                MessageAttributeNames=[
+                    'string',
+                ],
+                WaitTimeSeconds=2,
+                MaxNumberOfMessages=1
+            )
+
+            Worker.order = message[0].body
+
+            print ""
+            print "======order======"
+            print Worker.order
+            print "======order======"
+            print ""
+            response = queue.delete_messages(
+                Entries=[
+                    {
+                        'Id':'1',
+                        'ReceiptHandle': message[0].receipt_handle
+                    },
+                ]
+            )
+
+        except:
+            print "======order======"
+            print "None"
+            print "======order======"
+
+
+        if Worker.order:
+            return response
+        else:
+            return False
+
+    def control_servo(self):
+        """Control Servo it subject to Amazon SQS orders"""
+        print ""
+        print "=====Control Servo Process Start.====="
+        print ""
+        servo1.start(0.0)
+        servo2.start(0.0)
+        servo3.start(0.0)
+        servo4.start(0.0)
+
+        order = json.loads(Worker.order)
+
+        for key, value in order.items():
+
+            if key == "turn_right":
+                print key, value
+                Worker.current_point[0] = Worker.current_point[0] - value / float(1000)
+                if Worker.current_point[0] < 2.5 or Worker.current_point[0] > 12:
+                    Worker.current_point[0] = 6
+                servo4.ChangeDutyCycle(Worker.current_point[0])
+            if key == "turn_left":
+                print key, value
+                Worker.current_point[0] = Worker.current_point[0] - value / float(1000)
+                if Worker.current_point[0] < 2.5 or Worker.current_point[0] > 12:
+                    Worker.current_point[0] = 6
+                servo4.ChangeDutyCycle(Worker.current_point[0])
+            if key == "turn_top":
+                print key, value
+                Worker.current_point[1] = Worker.current_point[1] - value / float(1000)
+                if Worker.current_point[1] < 2.5 or Worker.current_point[1] > 12:
+                    Worker.current_point[1] = 2
+                servo1.ChangeDutyCycle(Worker.current_point[0])
+            if key == "turn_bottom":
+                print key, value
+                Worker.current_point[1] = Worker.current_point[1] - value / float(1000)
+                if Worker.current_point[1] < 2.5 or Worker.current_point[1] > 12:
+                    Worker.current_point[1] = 2
+                servo1.ChangeDutyCycle(Worker.current_point[1])
+
+        Worker.order = ''
+        print "Current point is"
+        print Worker.current_point[0], Worker.current_point[1]
+        time.sleep(1)
+        print "=====Servo Motor Turn Off.====="
+        print "=====Control Servo Process Ended.====="
+        print ""
+        print "Go Back to The Loop Top"
+        print ""
+
+
+if __name__ == "__main__":
+    while True:
+        Worker().publish_queue()
+        if Worker().get_order():
+            Worker().control_servo()
+        else:
+            continue
