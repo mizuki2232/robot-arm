3518ad552263626c424a4a48e9ec2585388e7c45
mizuki2232 <mizuki.kamata@serverworks.co.jp>
2017-08-29 21:34:21 +0900

    change directory

---
+++ script/dlib/train/tune_param.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in an image.  In
+#   particular, it shows how you can take a list of images from the command
+#   line and display each on the screen with red boxes overlaid on each human
+#   face.
+#
+#   The examples/faces folder contains some jpg images of people.  You can run
+#   this program on them and see the detections by executing the
+#   following command:
+#       ./face_detector.py ../examples/faces/*.jpg
+#
+#   This face detector is made using the now classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image
+#   pyramid, and sliding window detection scheme.  This type of object detector
+#   is fairly general and capable of detecting many types of semi-rigid objects
+#   in addition to human faces.  Therefore, if you are interested in making
+#   your own object detectors then read the train_object_detector.py example
+#   program.
+#
+#
+# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
+#   You can install dlib using the command:
+#       pip install dlib
+#
+#   Alternatively, if you want to compile dlib yourself then go into the dlib
+#   root folder and run:
+#       python setup.py install
+#   or
+#       python setup.py install --yes USE_AVX_INSTRUCTIONS
+#   if you have a CPU that supports AVX instructions, since this makes some
+#   things run faster.
+#
+#   Compiling dlib should work on any operating system so long as you have
+#   CMake and boost-python installed.  On Ubuntu, this can be done easily by
+#   running the command:
+#       sudo apt-get install libboost-python-dev cmake
+#
+#   Also note that this example requires scikit-image which can be installed
+#   via the command:
+#       pip install scikit-image
+#   Or downloaded from http://scikit-image.org/download.html.
+
+import cv2
+import boto3
+import dlib
+
+
+bucket_name = "bento-robot"
+detector = dlib.simple_object_detector("./detector.svm")
+s3 = boto3.resource('s3')
+win = dlib.image_window()
+c = cv2.VideoCapture(1)
+r, img = c.read()
+r = 200.0/img.shape[1]
+dimension = (200, int(img.shape[0] * r))
+img = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
+
+
+# The 1 in the second argument indicates that we should upsample the image
+# 1 time.  This will make everything bigger and allow us to detect more
+# faces.
+dets = detector(img, 1)
+print("Number of faces detected: {}".format(len(dets)))
+for i, d in enumerate(dets):
+    print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+        i, d.left(), d.top(), d.right(), d.bottom()))
+
+win.clear_overlay()
+win.set_image(img)
+win.add_overlay(dets)
+dlib.hit_enter_to_continue()
