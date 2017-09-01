0a2aa0440cd1cd71848e6d25283c40ebd5f74954
Mizuki Kamata <mizuki255@gmail.com>
2017-09-01 12:29:24 +0000

    add proof_of_quality.py

---
+++ script/dlib/train/proof_of_quality.py
@@ -0,0 +1,59 @@
+import base64
+from io import BytesIO
+import json
+from random import randint
+
+import boto3
+import cv2
+
+
+proof_image = "proof.jpg"
+bucket_name = "bento-robot"
+s3 = boto3.resource('s3')
+sqs = boto3.resource('sqs')
+queue_name = 'robot_arm'
+order = {}
+
+try:
+    order_queue = sqs.get_queue_by_name(QueueName='robot_arm_order')
+except:
+    order_queue = sqs.create_queue(QueueName='robot_arm_order')
+
+try:
+    image_queue = sqs.get_queue_by_name(QueueName='robot_arm_image')
+except:
+    image_queue = sqs.create_queue(QueueName='robot_arm_image')
+
+while True:
+    try:
+        message = image_queue.receive_messages(
+            AttributeNames=[
+                'All'
+            ],
+            MessageAttributeNames=[
+                'string',
+            ],
+            WaitTimeSeconds=3,
+            MaxNumberOfMessages=1
+        )
+        response = image_queue.delete_messages(
+            Entries=[
+                {
+                    'Id': '1',
+                    'ReceiptHandle': message[0].receipt_handle
+                },
+            ]
+        )
+
+        img = base64.b64decode(message[0].body)
+        img = np.fromstring(img, dtype=int)
+        r, img = cv2.imencode('.jpg', img)
+        img = BytesIO(img)
+        img = io.imread(img)
+        response = s3.Bucket(bucket_name).upload_fileobj(img, proof_image)
+
+    except (KeyboardInterrupt, SystemExit):
+        raise
+
+    except:
+        print "Back To The Loop Top"
