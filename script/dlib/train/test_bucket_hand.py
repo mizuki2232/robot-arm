2c1af2c720f2e43b9460dcfe2581147a4a56eaf0
mizuki2232 <mizuki.kamata@serverworks.co.jp>
2017-09-09 18:31:34 +0900

    add test bucket hand method

---
+++ script/dlib/train/test_bucket_hand.py
@@ -0,0 +1,33 @@
+import time
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+
+PIN = 23
+GPIO.setup(PIN, GPIO.OUT)
+servo1 = GPIO.PWM(PIN, 50)
+
+val = [2.5, 3.6875, 4.875, 6.0625, 7.25, 8.4375, 9.625, 10.8125, 12]
+
+
+def control_servo():
+    """controle servo method."""
+    print ""
+    print "=====Control Servo Process Start.====="
+    print ""
+    servo1.start(0.0)
+    servo1.ChangeDutyCycle(val[0])
+    time.sleep(3)
+    servo1.ChangeDutyCycle(val[8])
+    time.sleep(3)
+    servo1.ChangeDutyCycle(val[0])
+    print "=====Servo Motor Turn Off.====="
+    print "=====Control Servo Process Ended.====="
+    print ""
+    print "Go Back to The Loop Top"
+    print ""
+
+
+if __name__ == "__main__":
+    control_servo()
